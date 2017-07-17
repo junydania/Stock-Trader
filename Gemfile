2a3275820a315c0a0043988ec9f21ab31c8468aa
junydania <o.dania@aol.com>
2017-07-17 14:42:51 +0100

    Added the status of processing

--- Gemfile
+++ Gemfile
@@ -30,8 +30,6 @@ gem 'rails_12factor'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
 gem 'stock_quote'
-gem 'jquery-rails'
-
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery2
 //= require jquery_ujs
-//= require rails-ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+
+var hide_spinner = function() {
+    $('#spinner').hide();
+}
+
+var show_spinner = function() {
+    $('#spinner').show();
+};
+
 
